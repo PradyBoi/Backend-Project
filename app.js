ab99cbb68a9cd7cea12872e324ea7da81dbe2c40
PradyBoi <pradhumnkhare0340@gmail.com>
2022-08-22 11:27:15 +0530

    changesNumber2

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 // AuthRouter and QuotesRouter
-app.use('/', authRouter);
-app.use('/', quotesRouter);
+app.use(authRouter, quotesRouter);
 
 module.exports = app;
